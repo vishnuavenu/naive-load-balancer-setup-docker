c7d7e455839648fa7cd243629ebb322d550743f6
Vishnu A Venu <vishucool.venu@gmail.com>
2016-09-01 18:34:14 +0530

    Require to add env variable or define web interface with specified the ip address

---
+++ server3/app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import sys
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def reply():
+    return 'HELLO FROM FIRST SERVER 3'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
